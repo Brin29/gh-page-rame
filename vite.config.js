99e9a89941795d351d7297947eade8a92fc8698a
Brin29 <breinerstivenparracortes50@gmail.com>
2024-09-22 19:39:53 -0500

    cambio del path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Brin29/RAME/tree/main/RAME-PROJECT-FRONT-END/RAME-PROJECT"
+  base: "https://brin29.github.io/gh-page-rame"
 })
